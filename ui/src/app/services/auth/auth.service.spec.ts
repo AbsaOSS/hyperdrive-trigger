92115beab376b78ad56c9d06a50194ef0ad2488d
Kevin Wallimann <kevin.wallimann@absa.africa>
2020-02-28 15:47:32 +0100

    Fix tests

--- ui/src/app/services/auth/auth.service.spec.ts
+++ ui/src/app/services/auth/auth.service.spec.ts
@@ -16,12 +16,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
