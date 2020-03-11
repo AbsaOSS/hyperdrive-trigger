0c8b5303a373f4f331aa7a3426e8d4ea6b666eb3
Kevin Wallimann <kevin.wallimann@absa.africa>
2020-03-11 16:35:48 +0100

    Add configuration to package ui into jar with mvn package

--- src/main/scala/za/co/absa/hyperdrive/trigger/api/rest/WebSecurityConfig.scala
+++ src/main/scala/za/co/absa/hyperdrive/trigger/api/rest/WebSecurityConfig.scala
@@ -74,10 +74,18 @@ class WebSecurityConfig {
 
         .authorizeRequests()
         .antMatchers(
-          "/", "/index.html", "/css/**", "/webjars/**",
-          "/repository/**", "/Component.js", "/manifest.json",
-          "/i18n/**", "/model/**", "/view/**",
-          "/controller/**", "/lib/**", "/generic/**",
+          "/", "/index.html",
+          "/main-es5.*.js",
+          "/main-es2015.*.js",
+          "/polyfills-es5.*.js",
+          "/polyfills-es2015.*.js",
+          "/runtime-es5.*.js",
+          "/runtime-es2015.*.js",
+          "/scripts.*.js",
+          "/styles-es5.*.js",
+          "/styles-es2015.*.js",
+          "/vendor-es5.*.js",
+          "/vendor-es2015.*.js",
           "/favicon.ico"
         )
         .permitAll()
