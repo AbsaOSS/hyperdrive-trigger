29680b291932eed08531a9b87ee36cc2fd88a532
Kevin Wallimann <kevin.wallimann@absa.africa>
2020-04-07 10:07:17 +0200

    Fix bug time sensor only works half of a day (#136)

--- src/main/scala/za/co/absa/hyperdrive/trigger/scheduler/sensors/time/TimeSensorQuartzJob.scala
+++ src/main/scala/za/co/absa/hyperdrive/trigger/scheduler/sensors/time/TimeSensorQuartzJob.scala
@@ -16,12 +16,11 @@
 
 package za.co.absa.hyperdrive.trigger.scheduler.sensors.time
 
-import java.time.LocalDateTime
+import java.time.Instant
 import java.time.format.DateTimeFormatter
 
 import org.quartz.{Job, JobExecutionContext}
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
 import play.api.libs.json.JsObject
 import za.co.absa.hyperdrive.trigger.models.{Event, Properties}
 
@@ -29,14 +28,14 @@ import scala.concurrent.Future
 
 class TimeSensorQuartzJob extends Job {
   private val logger = LoggerFactory.getLogger(this.getClass)
-  private val eventDateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")
+  private val eventDateFormatter: DateTimeFormatter = DateTimeFormatter.ISO_INSTANT
 
   override def execute(jobExecutionContext: JobExecutionContext): Unit = {
     logger.info("Starting Time Sensor Quartz Job")
     val jobDataMap = jobExecutionContext.getJobDetail.getJobDataMap
     val push = jobDataMap.get(TimeSensor.PUSH_FUNCTION_JOB_DATA_MAP_KEY).asInstanceOf[Seq[Event] => Future[Unit]]
     val properties = jobDataMap.get(TimeSensor.PROPERTIES_JOB_DATA_MAP_KEY).asInstanceOf[Properties]
-    val sourceEventId = s"sid=${properties.sensorId};t=${eventDateFormatter.format(LocalDateTime.now())}"
+    val sourceEventId = s"sid=${properties.sensorId};t=${eventDateFormatter.format(Instant.now())}"
     val event = Event(sourceEventId, properties.sensorId, JsObject.empty)
     push(Seq(event))
   }
