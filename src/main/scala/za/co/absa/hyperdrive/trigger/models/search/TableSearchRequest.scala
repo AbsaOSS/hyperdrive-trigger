d8f5bd6307cc8c196fc9d9a501c7fa09aeded3fe
Senele Sithole <sitholesz88@gmail.com>
2020-04-16 14:18:08 +0200

    remove string filter component, back and front end

--- src/main/scala/za/co/absa/hyperdrive/trigger/models/search/TableSearchRequest.scala
+++ src/main/scala/za/co/absa/hyperdrive/trigger/models/search/TableSearchRequest.scala
@@ -16,7 +16,6 @@
 package za.co.absa.hyperdrive.trigger.models.search
 
 case class TableSearchRequest(
-  stringEqualsFilterAttributes: Option[Seq[StringEqualsFilterAttributes]] = None,
   containsFilterAttributes: Option[Seq[ContainsFilterAttributes]] = None,
   intRangeFilterAttributes: Option[Seq[IntRangeFilterAttributes]] = None,
   dateTimeRangeFilterAttributes: Option[Seq[DateTimeRangeFilterAttributes]] = None,
@@ -25,7 +24,6 @@ case class TableSearchRequest(
   from: Int,
   size: Int
 ) {
-  def getStringEqualsFilterAttributes: Seq[StringEqualsFilterAttributes] = stringEqualsFilterAttributes.getOrElse(Seq())
   def getContainsFilterAttributes: Seq[ContainsFilterAttributes] = containsFilterAttributes.getOrElse(Seq())
   def getIntRangeFilterAttributes: Seq[IntRangeFilterAttributes] = intRangeFilterAttributes.getOrElse(Seq())
   def getDateTimeRangeFilterAttributes: Seq[DateTimeRangeFilterAttributes] = dateTimeRangeFilterAttributes.getOrElse(Seq())
