9b89e64b3384930731f6c6b2995dc210cda55fb9
jozefbakus <jozef.bakus@gmail.com>
2020-04-30 10:37:50 +0200

    Uncommented working tests

--- ui/src/app/components/runs/filters/number-range-filter/number-range-filter.component.spec.ts
+++ ui/src/app/components/runs/filters/number-range-filter/number-range-filter.component.spec.ts
@@ -4,56 +4,56 @@ import { NumberRangeFilterComponent } from './number-range-filter.component';
 import { DagRunModel } from '../../../../models/dagRuns/dagRun.model';
 
 describe('NumberRangeFilterComponent', () => {
-  // let fixture: ComponentFixture<NumberRangeFilterComponent>;
-  //
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ NumberRangeFilterComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(NumberRangeFilterComponent);
-  // });
-  //
-  // it('should create', () => {
-  //   const underTest = fixture.componentInstance;
-  //   expect(underTest).toBeTruthy();
-  // });
-  //
-  // describe('accepts', () => {
-  //   it('should accept when it is in the range', () => {
-  //     const underTest = fixture.componentInstance;
-  //     underTest.value = {from: 1, to: 3};
-  //     underTest.property = 'jobCount';
-  //     const dagRun = new DagRunModel(
-  //       'value', 'projectName', 2, 'Status', new Date(Date.now()), new Date(Date.now()), 0
-  //     );
-  //
-  //     expect(underTest.accepts(dagRun)).toBeTrue();
-  //   });
-  //
-  //   it('should accept when it is on edge of the range', () => {
-  //     const underTest = fixture.componentInstance;
-  //     underTest.value = {from: 1, to: 2};
-  //     underTest.property = 'jobCount';
-  //     const dagRun = new DagRunModel(
-  //       'value', 'projectName', 2, 'Status', new Date(Date.now()), new Date(Date.now()), 0
-  //     );
-  //
-  //     expect(underTest.accepts(dagRun)).toBeTrue();
-  //   });
-  //
-  //   it('should not accept when it is not in the range', () => {
-  //     const underTest = fixture.componentInstance;
-  //     underTest.value = {from: 5, to: 8};
-  //     underTest.property = 'jobCount';
-  //     const dagRun = new DagRunModel(
-  //       'value', 'projectName', 2, 'Status', new Date(Date.now()), new Date(Date.now()), 0
-  //     );
-  //
-  //     expect(underTest.accepts(dagRun)).toBeFalse();
-  //   });
-  // });
+  let fixture: ComponentFixture<NumberRangeFilterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NumberRangeFilterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NumberRangeFilterComponent);
+  });
+
+  it('should create', () => {
+    const underTest = fixture.componentInstance;
+    expect(underTest).toBeTruthy();
+  });
+
+  describe('accepts', () => {
+    it('should accept when it is in the range', () => {
+      const underTest = fixture.componentInstance;
+      underTest.value = {from: 1, to: 3};
+      underTest.property = 'jobCount';
+      const dagRun = new DagRunModel(
+        'value', 'projectName', 2, 'Status', new Date(Date.now()), new Date(Date.now()), 0
+      );
+
+      expect(underTest.accepts(dagRun)).toBeTrue();
+    });
+
+    it('should accept when it is on edge of the range', () => {
+      const underTest = fixture.componentInstance;
+      underTest.value = {from: 1, to: 2};
+      underTest.property = 'jobCount';
+      const dagRun = new DagRunModel(
+        'value', 'projectName', 2, 'Status', new Date(Date.now()), new Date(Date.now()), 0
+      );
+
+      expect(underTest.accepts(dagRun)).toBeTrue();
+    });
+
+    it('should not accept when it is not in the range', () => {
+      const underTest = fixture.componentInstance;
+      underTest.value = {from: 5, to: 8};
+      underTest.property = 'jobCount';
+      const dagRun = new DagRunModel(
+        'value', 'projectName', 2, 'Status', new Date(Date.now()), new Date(Date.now()), 0
+      );
+
+      expect(underTest.accepts(dagRun)).toBeFalse();
+    });
+  });
 });
