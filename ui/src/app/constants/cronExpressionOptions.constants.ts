80989893bd65911e4a0a6e8c819093d3963fefb3
Senele Sithole <sitholesz88@gmail.com>
2020-05-12 17:16:00 +0200

    .

--- ui/src/app/constants/cronExpressionOptions.constants.ts
+++ ui/src/app/constants/cronExpressionOptions.constants.ts
@@ -13,23 +13,22 @@
  * limitations under the License.
  */
 
- export const cronExpressionOptions = {
-
-    MINUTES_VALUES : [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55],
-    MINUTE_VALUES : [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55],
-    HOUR_VALUES : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-    FREQUENCIES : [
-        {
-          value: 1,
-          label: 'Hour every',
-        },
-        {
-          value: 2,
-          label: 'Hour at',
-        },
-        {
-          value: 3,
-          label: 'Day',
-        },
-      ]
- };
+export const cronExpressionOptions = {
+  MINUTES_VALUES: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55],
+  MINUTE_VALUES: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55],
+  HOUR_VALUES: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+  FREQUENCIES: [
+    {
+      value: 1,
+      label: 'Hour every',
+    },
+    {
+      value: 2,
+      label: 'Hour at',
+    },
+    {
+      value: 3,
+      label: 'Day',
+    },
+  ],
+};
