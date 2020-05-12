6a6d2431c11f3011d84b4549cbfff72dad5ac327
Senele Sithole <sitholesz88@gmail.com>
2020-05-12 11:26:21 +0200

    get quartz cron expresion from view.

--- ui/src/app/components/workflows/workflow/dynamic-parts/cron-quartz-part/cron-quartz-part.component.spec.ts
+++ ui/src/app/components/workflows/workflow/dynamic-parts/cron-quartz-part/cron-quartz-part.component.spec.ts
@@ -1,25 +1,70 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
 import { CronQuartzPartComponent } from './cron-quartz-part.component';
+import { cronExpressionOptions } from '../../../../../constants/cronExpressionOptions.constants';
+import { WorkflowEntryModel } from '../../../../../models/workflowEntry.model';
 
-describe('CronQuartzPartComponent', () => {
-  // let component: CronQuartzPartComponent;
-  // let fixture: ComponentFixture<CronQuartzPartComponent>;
-  //
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ CronQuartzPartComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(CronQuartzPartComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-  //
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+fdescribe('CronQuartzPartComponent', () => {
+  let component: CronQuartzPartComponent;
+  let fixture: ComponentFixture<CronQuartzPartComponent>;
+  const frequencies = cronExpressionOptions.FREQUENCIES;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CronQuartzPartComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CronQuartzPartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set cron for every minutes', () => {
+    const underTest = fixture.componentInstance;
+    underTest.valueChanges = new Subject<WorkflowEntryModel>();
+    const minuteValue = 10;
+    const expectedMinuteCron = ['0', '0/10', '0', '?', '*', '*', '*'];
+    underTest.setCron(minuteValue, frequencies[0].label);
+
+    expect((underTest.cron).join(' ')).toEqual(expectedMinuteCron.join(' '));
+  });
+
+  it('should set cron for every hour', () => {
+    const underTest = fixture.componentInstance;
+    underTest.valueChanges = new Subject<WorkflowEntryModel>();
+    const hourValue = 20;
+    const expectedHourCron = ['0', '20', '0', '?', '*', '*', '*'];
+    underTest.setCron(hourValue, frequencies[1].label);
+
+    expect((underTest.cron).join(' ')).toEqual(expectedHourCron.join(' '));
+  });
+
+  it('should set cron for every day', () => {
+    const underTest = fixture.componentInstance;
+    underTest.valueChanges = new Subject<WorkflowEntryModel>();
+    const dayValue = 30;
+    const expectedDayCron = ['0', '0', '30', '?', '*', '*', '*'];
+    underTest.setCron(dayValue, frequencies[2].label);
+
+    expect((underTest.cron).join(' ')).toEqual(expectedDayCron.join(' '));
+  });
+
+  it('should set cron for to default midnight', () => {
+    const underTest = fixture.componentInstance;
+    underTest.valueChanges = new Subject<WorkflowEntryModel>();
+    const anyValue = 20;
+    const expectedDefaultCron = ['0', '0', '0', '?', '*', '*', '*'];
+    underTest.setCron(anyValue, 'default');
+
+    expect((underTest.cron).join(' ')).toEqual(expectedDefaultCron.join(' '));
+  });
+
 });
--- ui/src/app/components/workflows/workflow/dynamic-parts/cron-quartz-part/cron-quartz-part.component.ts
+++ ui/src/app/components/workflows/workflow/dynamic-parts/cron-quartz-part/cron-quartz-part.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { WorkflowEntryModel } from '../../../../../models/workflowEntry.model';
+import { cronExpressionOptions } from '../../../../../constants/cronExpressionOptions.constants';
 
 @Component({
   selector: 'app-cron-quartz-part',
@@ -16,165 +17,46 @@ export class CronQuartzPartComponent implements OnInit, AfterViewInit {
 
   base;
   dayValue;
-  dayOfMonthValue;
-  monthValue;
   minuteValue;
-  everyHourEvery;
-  // hourValue;
-  everyHourAt;
+  hourValue;
 
-  frequencies = [
-    {
-      value: 1,
-      label: 'Hour every',
-    },
-    {
-      value: 2,
-      label: 'Hour at',
-    },
-    {
-      value: 3,
-      label: 'Day',
-    },
-    {
-      value: 4,
-      label: 'Week',
-    },
-    {
-      value: 5,
-      label: 'Month',
-    },
-    {
-      value: 6,
-      label: 'Year',
-    },
-  ];
-  dayValues = [
-    {
-      value: 1,
-      label: 'Monday',
-    },
-    {
-      value: 2,
-      label: 'Tuesday',
-    },
-    {
-      value: 3,
-      label: 'Wednesday',
-    },
-    {
-      value: 4,
-      label: 'Thursday',
-    },
-    {
-      value: 5,
-      label: 'Friday',
-    },
-    {
-      value: 6,
-      label: 'Saturday',
-    },
-    {
-      value: 7,
-      label: 'Sunday',
-    },
-  ];
-  dayOfMonthValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  monthValues = [
-    {
-      value: 1,
-      label: 'January',
-    },
-    {
-      value: 2,
-      label: 'February',
-    },
-    {
-      value: 3,
-      label: 'March',
-    },
-    {
-      value: 4,
-      label: 'April',
-    },
-    {
-      value: 5,
-      label: 'May',
-    },
-    {
-      value: 6,
-      label: 'June',
-    },
-    {
-      value: 7,
-      label: 'July',
-    },
-    {
-      value: 8,
-      label: 'August',
-    },
-    {
-      value: 9,
-      label: 'September',
-    },
-    {
-      value: 10,
-      label: 'October',
-    },
-    {
-      value: 11,
-      label: 'November',
-    },
-    {
-      value: 12,
-      label: 'December',
-    },
-  ];
-  hourValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-  minuteValues = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
-  minutesValues = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+
+  hourValues = cronExpressionOptions.HOUR_VALUES;
+  frequencies = cronExpressionOptions.FREQUENCIES;
+  minuteValues = cronExpressionOptions.MINUTE_VALUES;
+  minutesValues = cronExpressionOptions.MINUTES_VALUES;
+  cron: string[] = [];
 
   constructor() {
-    // do nothing
   }
 
   ngOnInit(): void {
-    this.fromCron(this.value);
   }
 
   ngAfterViewInit(): void {
-    // this.fromCron(this.value)
   }
 
-  setCron(n): string {
-    const cron = ['0', '*', '*', '*', '*', '?'];
-
-    if (n && n.base && n.base >= 2) {
-      cron[1] = typeof n.minuteValue !== undefined ? n.minuteValue : '0';
+  setCron(value, label) {
+    this.cron = ['0', '0', '0', '?', '*', '*', '*'];
+    switch (label) {
+      case 'Hour every':
+        this.cron[1] = `0/${value}`;
+        break;
+      case 'Hour at':
+        this.cron[1] = value;
+        break;
+      case 'Day':
+        this.cron[2] = value;
+        break;
+      default:
+        this.cron = ['0', '0', '0', '?', '*', '*', '*'];
+        break;
     }
-
-    if (n && n.base && n.base >= 3) {
-      cron[2] = typeof n.hourValue !== undefined ? n.hourValue : '*';
-    }
-
-    if (n && n.base && n.base === 4) {
-      cron[3] = '?';
-      cron[5] = n.dayValue;
-    }
-
-    if (n && n.base && n.base >= 5) {
-      cron[3] = typeof n.dayOfMonthValue !== undefined ? n.dayOfMonthValue : '?';
-    }
-
-    if (n && n.base && n.base === 6) {
-      cron[4] = typeof n.monthValue !== undefined ? n.monthValue : '*';
-    }
-    return cron.join(' ');
+    this.modelChanged();
   }
 
   fromCron(value: string) {
     const cron: string[] = value.replace(/\s+/g, ' ').split(' ');
-    // var frequency = {base: '1'}; // default: every minute
 
     if (cron[1] === '*' && cron[2] === '*' && cron[3] === '*' && cron[4] === '*' && cron[5] === '?') {
       this.base = 1; // every minute
@@ -182,32 +64,27 @@ export class CronQuartzPartComponent implements OnInit, AfterViewInit {
       this.base = 2; // every hour
     } else if (cron[3] === '*' && cron[4] === '*' && cron[5] === '?') {
       this.base = 3; // every day
-    } else if (cron[3] === '?') {
-      this.base = 4; // every week
-    } else if (cron[4] === '*' && cron[5] === '?') {
-      this.base = 5; // every month
-    } else if (cron[5] === '?') {
-      this.base = 6; // every year
     }
+  }
 
-    // if (cron[1] !== '*') {
-    //   this.minuteValue = parseInt(cron[1]);
-    // }
-    // if (cron[2] !== '*') {
-    //   this.hourValue = parseInt(cron[2]);
-    // }
-    // if (cron[3] !== '*' && cron[3] !== '?') {
-    //   this.dayOfMonthValue = parseInt(cron[3]);
-    // }
-    // if (cron[4] !== '*') {
-    //   this.monthValue = parseInt(cron[4]);
-    // }
-    // if (cron[5] !== '*' && cron[5] !== '?') {
-    //   this.dayValue = parseInt(cron[5]);
-    // }
-    // //
-    // frequency.base += ''; // 'cast' to string in order to set proper value on "every" modal
-    // //
-    // return frequency;
+  onMinuteSelect(option) {
+    this.minuteValue = option;
+    this.setCron(this.minuteValue, this.frequencies[0].label);
+  }
+
+  onHourSelect(option) {
+    this.hourValue = option;
+    this.setCron(this.hourValue, this.frequencies[1].label);
+  }
+
+  onDaySelect(option) {
+    this.dayValue = option;
+    this.setCron(this.dayValue, this.frequencies[2].label);
+  }
+
+  modelChanged() {
+    this.value = this.cron.join(' ');
+    console.log('about to send this cron expression: ' + this.value);
+    this.valueChanges.next(new WorkflowEntryModel(this.property, this.value));
   }
 }
