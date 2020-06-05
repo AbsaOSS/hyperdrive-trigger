d4c36391161103c07a79777c718cc5665da52eb6
jozefbakus <jozef.bakus@gmail.com>
2020-06-05 10:48:11 +0200

    Quartz service

---
+++ ui/src/app/services/util/util.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { api } from '../../constants/api.constants';
+import { map } from 'rxjs/operators';
+import { QuartzExpressionDetailModel } from '../../models/quartzExpressionDetail.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilService {
+  constructor(private httpClient: HttpClient) {}
+
+  getQuartzDetail(expression: string): Observable<QuartzExpressionDetailModel> {
+    const params = new HttpParams().set('expression', expression);
+
+    return this.httpClient
+      .get<QuartzExpressionDetailModel>(api.GET_QUARTZ_DETAIL, { params: params, observe: 'response' })
+      .pipe(map((_) => _.body));
+  }
+}
