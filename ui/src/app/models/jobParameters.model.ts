b196fdcd0f1df451f846e71587d5344cbcf6d2cd
jozefbakus <jozef.bakus@gmail.com>
2020-12-09 15:56:52 +0100

    Frontend - jobParameters model extracted

---
+++ ui/src/app/models/jobParameters.model.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export type JobParametersModel = {
+  variables: Map<string, string>;
+  maps: Map<string, Set<string>>;
+  keyValuePairs: Map<string, Map<string, string>>;
+};
+
+export class JobParametersModelFactory {
+  static create(
+    variables: Map<string, string>,
+    maps: Map<string, Set<string>>,
+    keyValuePairs: Map<string, Map<string, string>>,
+  ): JobParametersModel {
+    return { variables: variables, maps: maps, keyValuePairs: keyValuePairs };
+  }
+
+  static createEmpty(): JobParametersModel {
+    return this.create(new Map(), new Map(), new Map());
+  }
+}
