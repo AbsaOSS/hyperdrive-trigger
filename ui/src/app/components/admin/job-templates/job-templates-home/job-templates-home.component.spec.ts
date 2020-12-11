33d7511d311040f41d08f2a07352335f21ea5465
jozefbakus <jozef.bakus@gmail.com>
2020-12-11 15:23:06 +0100

    PR fix

--- ui/src/app/components/admin/job-templates/job-templates-home/job-templates-home.component.spec.ts
+++ ui/src/app/components/admin/job-templates/job-templates-home/job-templates-home.component.spec.ts
@@ -19,13 +19,29 @@ import { JobTemplatesHomeComponent } from './job-templates-home.component';
 import { provideMockStore } from '@ngrx/store/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { absoluteRoutes } from '../../../../constants/routes.constants';
+import { JobTemplateModelFactory } from '../../../../models/jobTemplate.model';
+import { JobParametersModelFactory } from '../../../../models/jobParameters.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../stores/app.reducers';
+import { ClrDatagridStateInterface } from '@clr/angular';
 
 describe('JobTemplatesHomeComponent', () => {
   let underTest: JobTemplatesHomeComponent;
   let fixture: ComponentFixture<JobTemplatesHomeComponent>;
   let router: Router;
+  let store: Store<AppState>;
 
-  const initialAppState = {};
+  const initialAppState = {
+    jobTemplates: {
+      jobTemplates: [
+        JobTemplateModelFactory.create(0, 'templateName', 'fromConfig', { name: 'jobType' }, JobParametersModelFactory.createEmpty()),
+      ],
+      total: 2,
+      page: 3,
+      loading: false,
+    },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,6 +50,7 @@ describe('JobTemplatesHomeComponent', () => {
       imports: [RouterTestingModule.withRoutes([])],
     }).compileComponents();
     router = TestBed.inject(Router);
+    store = TestBed.inject(Store);
   }));
 
   beforeEach(() => {
@@ -45,4 +62,55 @@ describe('JobTemplatesHomeComponent', () => {
   it('should create', () => {
     expect(underTest).toBeTruthy();
   });
+
+  it('onClarityDgRefresh() should dispatch SearchJobTemplates action', async(() => {
+    const storeSpy = spyOn(store, 'dispatch');
+    const removeFiltersSubjectSpy = spyOn(underTest.refreshSubject, 'next');
+    const clrDatagridState: ClrDatagridStateInterface = {
+      page: {
+        from: 1,
+        to: 2,
+        size: 10,
+        current: 3,
+      },
+      sort: {
+        by: 'by',
+        reverse: false,
+      },
+      filters: [],
+    };
+
+    underTest.onClarityDgRefresh(clrDatagridState);
+
+    expect(removeFiltersSubjectSpy).toHaveBeenCalledTimes(1);
+    expect(storeSpy).toHaveBeenCalledTimes(1);
+  }));
+
+  it('refresh() should dispatch SearchJobTemplates action', async(() => {
+    const storeSpy = spyOn(store, 'dispatch');
+    const removeFiltersSubjectSpy = spyOn(underTest.refreshSubject, 'next');
+
+    underTest.refresh();
+
+    expect(removeFiltersSubjectSpy).toHaveBeenCalledTimes(1);
+    expect(storeSpy).toHaveBeenCalledTimes(1);
+  }));
+
+  it('clearFilters() should call next on removeFiltersSubject', async(() => {
+    const removeFiltersSubjectSpy = spyOn(underTest.removeFiltersSubject, 'next');
+
+    underTest.clearFilters();
+
+    expect(removeFiltersSubjectSpy).toHaveBeenCalledTimes(1);
+  }));
+
+  it('showJobTemplate() should navigate to show job template page', async(() => {
+    const id = 42;
+    const routerSpy = spyOn(router, 'navigate');
+
+    underTest.showJobTemplate(id);
+
+    expect(routerSpy).toHaveBeenCalledTimes(1);
+    expect(routerSpy).toHaveBeenCalledWith([absoluteRoutes.SHOW_JOB_TEMPLATE, id]);
+  }));
 });
