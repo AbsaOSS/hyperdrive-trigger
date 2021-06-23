954a2f63501ed5337c2eb8b65be95fee2c284e2b
Kevin Wallimann <kevin.wallimann@absa.africa>
2021-06-23 17:19:48 +0200

    Add timeout config for yarn connection health indicator

--- src/main/scala/za/co/absa/hyperdrive/trigger/scheduler/utilities/Configs.scala
+++ src/main/scala/za/co/absa/hyperdrive/trigger/scheduler/utilities/Configs.scala
@@ -122,8 +122,11 @@ object JobDefinitionConfig {
 object HealthConfig {
   lazy val databaseConnectionTimeoutMillis: Int =
     Try(Configs.conf.getInt("health.databaseConnection.timeoutMillis")).getOrElse(120000)
-  lazy val yarnConnectionTestEndpoint: String =
+  lazy val yarnConnectionTestEndpoint: String = {
     Configs.conf.getString("health.yarnConnection.testEndpoint")
+  }
+  lazy val yarnConnectionTimeoutMillisOpt: Option[Int] =
+    Try(Configs.conf.getInt("health.yarnConnection.timeoutMillis")).toOption
 }
 
 object ApplicationConfig {
