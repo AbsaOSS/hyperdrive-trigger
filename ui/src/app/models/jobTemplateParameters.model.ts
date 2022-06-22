c8f0b4636a52f555f231490f181147a9759ee6ba
jozefbakus <jozef.bakus@gmail.com>
2022-06-22 13:25:17 +0200

    #689 - UX - models updated

--- ui/src/app/models/jobTemplateParameters.model.ts
+++ ui/src/app/models/jobTemplateParameters.model.ts
@@ -14,6 +14,7 @@
  */
 
 import { jobTypes } from '../constants/jobTypes.constants';
+import { KeyValueModel } from "./keyValue.model";
 
 export interface JobTemplateParameters {
   jobType: string;
@@ -24,7 +25,7 @@ export class SparkTemplateParametersModel implements JobTemplateParameters {
   appArguments: string[];
   additionalJars: string[];
   additionalFiles: string[];
-  additionalSparkConfig: Map<string, string>;
+  additionalSparkConfig: KeyValueModel[];
   jobJar: string;
   mainClass: string;
 
@@ -34,7 +35,7 @@ export class SparkTemplateParametersModel implements JobTemplateParameters {
       appArguments: [],
       additionalJars: [],
       additionalFiles: [],
-      additionalSparkConfig: new Map(),
+      additionalSparkConfig: [],
       jobJar: '',
       mainClass: '',
     };
