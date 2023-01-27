eea92f50e055f73fa9419ae0523f5fe2b6718ff8
jozefbakus <jozef.bakus@gmail.com>
2023-01-27 10:44:09 +0100

    #752: UI warnings fixes

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -27,13 +27,10 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    // "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
         "argsIgnorePattern": "_",
-        "varsIgnorePattern": "_",
-        "caughtErrorsIgnorePattern": "_"
       }
     ]
   }
