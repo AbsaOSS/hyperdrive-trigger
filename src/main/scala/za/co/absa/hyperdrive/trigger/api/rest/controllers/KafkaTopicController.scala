81914a6f209d2197628d9b9f664c3cb387a1227c
jozefbakus <jozef.bakus@gmail.com>
2023-02-08 14:17:22 +0100

    Controller

--- src/main/scala/za/co/absa/hyperdrive/trigger/api/rest/controllers/KafkaTopicController.scala
+++ src/main/scala/za/co/absa/hyperdrive/trigger/api/rest/controllers/KafkaTopicController.scala
@@ -15,23 +15,19 @@
 
 package za.co.absa.hyperdrive.trigger.api.rest.controllers
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import za.co.absa.hyperdrive.trigger.api.rest.services.DatasetService
-import za.co.absa.hyperdrive.trigger.models.VersionedDataset
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RestController}
+import za.co.absa.hyperdrive.trigger.api.rest.services.KafkaTopicService
+import za.co.absa.hyperdrive.trigger.models.KafkaTopicAuthorizationResponse
 
-import java.util.Optional
 import java.util.concurrent.CompletableFuture
 import javax.inject.Inject
 import scala.compat.java8.FutureConverters._
-import scala.compat.java8.OptionConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @RestController
-class DatasetController @Inject() (datasetService: DatasetService) {
+class KafkaTopicController @Inject() (kafkaTopicService: KafkaTopicService) {
 
-  @GetMapping(path = Array("/dataset/search"))
-  def search(@RequestParam searchQuery: Optional[String]): CompletableFuture[Seq[VersionedDataset]] =
-    datasetService.search(searchQuery.asScala).toJava.toCompletableFuture
+  @GetMapping(path = Array("/kafka/{kafkaTopic}/authorizations"))
+  def getKafkaTopicAuthorizations(@PathVariable kafkaTopic: String): CompletableFuture[KafkaTopicAuthorizationResponse] =
+    kafkaTopicService.getKafkaTopicAuthorizations(kafkaTopic).toJava.toCompletableFuture
 }
