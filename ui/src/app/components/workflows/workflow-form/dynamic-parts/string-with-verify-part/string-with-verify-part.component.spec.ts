f75f9ea7617569b663858ea4f936bb8a7ede7999
jozefbakus <jozef.bakus@gmail.com>
2023-02-09 16:06:53 +0100

    Component

---
+++ ui/src/app/components/workflows/workflow-form/dynamic-parts/string-with-verify-part/string-with-verify-part.component.spec.ts
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { StringWithVerifyPartComponent } from './string-with-verify-part.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement, Predicate } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+describe('StringWithVerifyPartComponent', () => {
+  let fixture: ComponentFixture<StringWithVerifyPartComponent>;
+  let underTest: StringWithVerifyPartComponent;
+
+  const inputSelector: Predicate<DebugElement> = By.css('input[type="text"]');
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [StringWithVerifyPartComponent],
+        imports: [FormsModule],
+        providers: [NgForm],
+      }).compileComponents();
+    }),
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StringWithVerifyPartComponent);
+    underTest = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(underTest).toBeTruthy();
+  });
+
+  describe('should set empty string on init when value is undefined or null', () => {
+    const parameters = [null, undefined];
+
+    parameters.forEach((parameter) => {
+      it(
+        'should pass with ' + parameter + ' value',
+        waitForAsync(() => {
+          const oldValue = parameter;
+          const newValue = '';
+          spyOn(underTest.valueChange, 'emit');
+
+          underTest.isShow = false;
+          underTest.isRequired = true;
+          underTest.name = 'name';
+          underTest.value = oldValue;
+          fixture.detectChanges();
+
+          fixture.whenStable().then(() => {
+            const result = fixture.debugElement.query(inputSelector).nativeElement.value;
+            expect(result).toBe(newValue);
+            expect(underTest.valueChange.emit).toHaveBeenCalled();
+            expect(underTest.valueChange.emit).toHaveBeenCalledWith(newValue);
+          });
+        }),
+      );
+    });
+  });
+
+  it(
+    'should change value and emit change on user input',
+    waitForAsync(() => {
+      const oldValue = 'oldValue';
+      const newValue = 'newValue';
+      spyOn(underTest.valueChange, 'emit');
+
+      underTest.isShow = false;
+      underTest.name = 'name';
+      underTest.value = oldValue;
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        const inputElement = fixture.debugElement.query(inputSelector).nativeElement;
+
+        inputElement.value = newValue;
+        inputElement.dispatchEvent(new Event('input'));
+
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          const testedValue = fixture.debugElement.query(inputSelector).nativeElement.value;
+          expect(testedValue).toBe(newValue);
+          expect(underTest.valueChange.emit).toHaveBeenCalled();
+          expect(underTest.valueChange.emit).toHaveBeenCalledWith(newValue);
+        });
+      });
+    }),
+  );
+});
---
+++ ui/src/app/components/workflows/workflow-form/dynamic-parts/string-with-verify-part/string-with-verify-part.component.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
+import { UuidUtil } from '../../../../../utils/uuid/uuid.util';
+import { texts } from 'src/app/constants/texts.constants';
+
+@Component({
+  selector: 'app-string-with-verify-part',
+  templateUrl: './string-with-verify-part.component.html',
+  styleUrls: ['./string-with-verify-part.component.scss'],
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
+})
+export class StringWithVerifyPartComponent implements OnInit {
+  uiid = UuidUtil.createUUID();
+  @Input() isShow: boolean;
+  @Input() name: string;
+  @Input() value: string;
+  @Input() verifyButtonText: string;
+  @Input() verifyCallback: (arg: string) => void;
+  @Output() valueChange = new EventEmitter();
+  @Input() isRequired = false;
+  @Input() minLength = 1;
+  @Input() maxLength: number = Number.MAX_SAFE_INTEGER;
+  @Input() helperText: string;
+
+  texts = texts;
+
+  constructor() {
+    // do nothing
+  }
+
+  modelChanged(value: string) {
+    this.valueChange.emit(value.trim());
+  }
+
+  verify(): void {
+    this.verifyCallback(this.value)
+  }
+
+  ngOnInit(): void {
+    if (!this.value && this.isRequired) {
+      this.modelChanged('');
+    }
+  }
+}
