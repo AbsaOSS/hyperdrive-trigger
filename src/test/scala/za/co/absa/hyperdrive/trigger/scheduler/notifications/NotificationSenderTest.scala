03fca8ed51c8834c1e7f061c59b80cd22f5a3e88
Kevin Wallimann <kevin.wallimann@absa.africa>
2024-01-12 13:24:05 +0100

    Feature/819 store error messages (#827)
    * Add error_message field to DB
    
    * Store diagnostics in job instance table
    
    * Rename error_message -> diagnostics
    
    * Add diagnostics to notification
    
    * List all causes
    
    * Add bulletpoints
    
    * Fixes
    
    * Fix format

--- src/test/scala/za/co/absa/hyperdrive/trigger/scheduler/notifications/NotificationSenderTest.scala
+++ src/test/scala/za/co/absa/hyperdrive/trigger/scheduler/notifications/NotificationSenderTest.scala
@@ -131,6 +131,75 @@ class NotificationSenderTest extends FlatSpec with MockitoSugar with Matchers wi
     messagesCaptor.getValue should include(s"Failed application: $clusterBaseUrl/cluster/app/application_9876_4567")
   }
 
+  it should "print the error message if diagnostics are available" in {
+    // given
+    val di = createDagInstance().copy(
+      status = DagInstanceStatuses.Failed,
+      started = LocalDateTime.of(LocalDate.of(2020, 3, 2), LocalTime.of(12, 30)),
+      finished = Some(LocalDateTime.of(LocalDate.of(2020, 3, 2), LocalTime.of(14, 30)))
+    )
+
+    val diagnostics =
+      """User class threw exception: za.co.absa.hyperdrive.shared.exceptions.IngestionException: PROBABLY FAILED INGESTION
+        |Caused by: org.apache.spark.sql.streaming.StreamingQueryException: Job aborted.
+        |=== Streaming Query ===
+        |Caused by: org.apache.spark.SparkException: Job aborted.
+        |at org.apache.spark.sql.execution.datasources.FileFormatWriter$.write(FileFormatWriter.scala:198)
+        |Caused by: java.lang.IllegalStateException: 29.compact doesn't exist when compacting batch 39
+        |at org.apache.spark.sql.execution.streaming.CompactibleFileStreamLog$$anonfun$4$$anonfun$apply$1.apply(CompactibleFileStreamLog.scala:174)
+        |""".stripMargin
+    val ji =
+      createJobInstance().copy(
+        jobStatus = JobStatuses.Failed,
+        applicationId = Some("application_1234_4567"),
+        order = 1,
+        diagnostics = Some(diagnostics)
+      )
+
+    val nr1 = createNotificationRule().copy(id = 1, recipients = Seq("abc@def.com", "xyz@def.com"))
+    val w = createWorkflow()
+
+    when(notificationRuleService.getMatchingNotificationRules(eqTo(di.workflowId), eqTo(di.status))(any()))
+      .thenReturn(Future(Some(Seq(nr1), w)))
+
+    // when
+    await(underTest.createNotifications(di, Seq(ji)))
+    underTest.sendNotifications()
+
+    // then
+    val expectedMessage =
+      """Environment: TEST
+        |Project: project
+        |Workflow Name: workflow
+        |Started: 2020-03-02T12:30:00
+        |Finished: 2020-03-02T14:30:00
+        |Status: Failed
+        |Failed application: http://localhost:8088/cluster/app/application_1234_4567
+        |Notification rule ID: 1
+        |
+        |Causes:
+        |- org.apache.spark.sql.streaming.StreamingQueryException: Job aborted.
+        |- org.apache.spark.SparkException: Job aborted.
+        |- java.lang.IllegalStateException: 29.compact doesn't exist when compacting batch 39
+        |
+        |Stack trace:
+        |User class threw exception: za.co.absa.hyperdrive.shared.exceptions.IngestionException: PROBABLY FAILED INGESTION
+        |Caused by: org.apache.spark.sql.streaming.StreamingQueryException: Job aborted.
+        |=== Streaming Query ===
+        |Caused by: org.apache.spark.SparkException: Job aborted.
+        |at org.apache.spark.sql.execution.datasources.FileFormatWriter$.write(FileFormatWriter.scala:198)
+        |Caused by: java.lang.IllegalStateException: 29.compact doesn't exist when compacting batch 39
+        |at org.apache.spark.sql.execution.streaming.CompactibleFileStreamLog$$anonfun$4$$anonfun$apply$1.apply(CompactibleFileStreamLog.scala:174)
+        |
+        |
+        |This message has been generated automatically. Please don't reply to it.
+        |
+        |HyperdriveDevTeam""".stripMargin
+    val messagesCaptor: ArgumentCaptor[String] = ArgumentCaptor.forClass(classOf[String])
+    verify(emailService).sendMessageToBccRecipients(any(), any(), any(), messagesCaptor.capture())
+    messagesCaptor.getValue shouldBe expectedMessage
+  }
+
   it should "retry sending the message at most maxRetries times" in {
     // given
     val maxRetries = 5
